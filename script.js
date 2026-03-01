8662773050904ef3febc59566edb2a2997747d96
Mohit-beep101git config --global user.name Mohit-beep101git config --global user.name Mohit-beep101 <mohitff9@gmail.com>
2026-03-01 16:59:39 +0530

    Day 12:Javascipt basics and variables

---
+++ script.js
@@ -0,0 +1,33 @@
+let name = "Mohit Kumar";
+let age = 21;
+let isStudent = true
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Is Student: " + isStudent);
+
+console.log(typeof name); // string
+console.log(typeof age); // number  
+console.log(typeof isStudent); // boolean
+
+if(age>=18){
+    console.log("eligible to vote");
+
+}
+else{
+    console.log("not eligible to vote");
+}
+
+let college = "St. Xavier's College";
+let course = "BCA";
+
+console.log("College name:" + college);
+console.log("Course name:" +course);
+
+if(course ==="BCA"){
+    console.log("Mohit is a BCA student");
+
+}
+else{
+    console.log("Mohit is not a BCA student");
+}
